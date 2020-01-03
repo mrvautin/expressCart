f96d8335d67e867b1e92f8ab4db23dce92a90b9e
Mark Moffat <mark@Marks-MacBook-Pro-2.local>
2020-01-03 22:59:00 +1030

    Better display of cart amount breakdown

--- app.js
+++ app.js
@@ -290,6 +290,18 @@ handlebars = handlebars.create({
             return value.replace(/^\w/, (chr) => {
                 return chr.toUpperCase();
             });
+        },
+        math: (lvalue, operator, rvalue, options) => {
+            lvalue = parseFloat(lvalue);
+            rvalue = parseFloat(rvalue);
+
+            return{
+                '+': lvalue + rvalue,
+                '-': lvalue - rvalue,
+                '*': lvalue * rvalue,
+                '/': lvalue / rvalue,
+                '%': lvalue % rvalue
+            }[operator];
         }
     }
 });
