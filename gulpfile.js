8373dfc430a6e84ec8d78eb7ea4346c84c0d0a3a
Mark Moffat <mark@Marks-MacBook-Pro-2.local>
2020-02-01 11:21:37 +1030

    Setup gulp watch for dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
-const { src, dest, series } = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
 const less = require('gulp-less');
+const colors = require('colors');
 const cleanCSS = require('gulp-clean-css');
 const minify = require('gulp-minify');
 const rename = require('gulp-rename');
+
+const nodemonOptions = {
+    script: 'app.js',
+    ext: 'js json',
+    env: { NODE_ENV: 'development' },
+    verbose: false,
+    ignore: [],
+    watch: ['lib/*', 'config/*', 'routes/*', 'app.js']
+};
 
 function lessCss(){
     return gulp.src('public/stylesheets/less/**/*.less')
@@ -74,3 +85,17 @@ function compressThemeJS(){
 
 // run the tasks
 gulp.task('default', series(lessCss, compressJS, compressCss, compressThemeCss, compressThemeJS));
+
+gulp.task('watch', (done) => {
+    // Watch LESS files and generate CSS
+    watch(['public/stylesheets/less/**/*.less'], async () => {
+        lessCss();
+        console.log(colors.blue('CSS generation complete'));
+    });
+
+    // run, watch and restart app
+    nodemon(nodemonOptions)
+    .once('quit', () => {
+        done();
+    });
+});
