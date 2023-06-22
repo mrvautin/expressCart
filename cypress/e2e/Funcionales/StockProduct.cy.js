293d899b6a2f188f3c44af0fee8ef0bd3c0d2920
Juan David Olaya <66858283+ZimaBlue7@users.noreply.github.com>
2023-06-22 11:51:07 -0500

    Update StockProduct.cy.js

--- cypress/e2e/Funcionales/StockProduct.cy.js
+++ cypress/e2e/Funcionales/StockProduct.cy.js
@@ -1,7 +1,12 @@
 /* eslint-disable no-undef */
 describe('Valor limite', () => {
-  it('passes', () => {
-    cy.visit('/admin');
+  beforeEach(() => {
+    cy.visit('http://localhost:1111/');
+    cy.get(':nth-child(1) > .thumbnail > p.text-center > .btn').click();
+    cy.get('#product_quantity').clear();
+  });
+  it('Comprobacion', () => {
+    cy.visit('http://localhost:1111/admin');
     cy.get('#email').type('juan.getial@correounivalle.edu.co');
     cy.get('#password').type('199656');
     cy.get('#loginForm').click();
@@ -10,38 +15,54 @@ describe('Valor limite', () => {
     cy.get(
       '#product_opt_wrapper > :nth-child(2) > .row > :nth-child(3)'
     ).should('contain', '5');
-
-    cy.visit('/');
-    cy.get(':nth-child(1) > .thumbnail > p.text-center > .btn').click();
-    cy.get('#product_quantity').clear();
+  });
+  it('Valor0', () => {
     cy.get('#product_quantity').type('0');
     // automaticamente al intentar añadir al carrito con 0 cantidad lo pasa a 1
     cy.get('#product_quantity').should('have.value', '0');
+    cy.get('.btnAddToCart > .btn').click();
+    cy.get('.navbar-nav > :nth-child(3) > .btn').click();
+    cy.get('.mt-md-4').should('contain', '0');
+  });
 
-    cy.get('#product_quantity').clear();
+  it('Valor1', () => {
     cy.get('#product_quantity').type('1');
     // Si deja agregar correctamente
     cy.get('#product_quantity').should('have.value', '1');
+    cy.get('.btnAddToCart > .btn').click();
+    cy.get('.navbar-nav > :nth-child(3) > .btn').click();
+  });
 
-    cy.get('#product_quantity').clear();
+  it('Valor2', () => {
     cy.get('#product_quantity').type('2');
     // Si deja agregar correctamente el valor esperado
     cy.get('#product_quantity').should('have.value', '2');
+    cy.get('.btnAddToCart > .btn').click();
+    cy.get('.navbar-nav > :nth-child(3) > .btn').click();
+  });
 
-    cy.get('#product_quantity').clear();
+  it('Valor4', () => {
     cy.get('#product_quantity').type('4');
     // Si deja agregar correctamente el valor esperado
     cy.get('#product_quantity').should('have.value', '4');
+    cy.get('.btnAddToCart > .btn').click();
+    cy.get('.navbar-nav > :nth-child(3) > .btn').click();
+  });
 
-    cy.get('#product_quantity').clear();
+  it('Valor5', () => {
     cy.get('#product_quantity').type('5');
     // Si deja agregar correctamente el valor esperado
     cy.get('#product_quantity').should('have.value', '5');
+    cy.get('.btnAddToCart > .btn').click();
+    cy.get('.navbar-nav > :nth-child(3) > .btn').click();
+  });
 
-    cy.get('#product_quantity').clear();
+  it('Valor6', () => {
     cy.get('#product_quantity').type('6');
     // deja agregar 6 pero no deberai ya que en stock solo hay 5 de cantidad
     cy.get('.btnAddToCart > .btn').click();
     cy.get('#product_quantity').should('have.value', '6');
+    cy.get('.btnAddToCart > .btn').click();
+    cy.get('.navbar-nav > :nth-child(3) > .btn').click();
   });
 });
