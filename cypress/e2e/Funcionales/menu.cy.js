41c434d71152869f2fe36abc5f350f7477ed5965
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-06-24 18:00:14 -0500

    Fixing tests

--- cypress/e2e/Funcionales/menu.cy.js
+++ cypress/e2e/Funcionales/menu.cy.js
@@ -1,11 +1,9 @@
+/* eslint-disable spaced-comment */
+/* eslint-disable extra-rules/no-commented-out-code */
 /* eslint-disable no-undef */
 /* eslint-disable quotes */
 describe("Prueba cambio de nombre de la tienda", () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit("http://localhost:1111/admin");
   });
   it("Campos aceptables", () => {
@@ -13,29 +11,49 @@ describe("Prueba cambio de nombre de la tienda", () => {
     cy.get("#password").type("admin12");
     cy.get("#loginForm").click();
     cy.get(".mb-2 > :nth-child(2) > .nav-link").click();
-    cy.get("#newNavMenu");
-    cy.get("#newNavLink");
-    cy.get("#settings-menu-new");
-    cy.get("#notify_message");
+    cy.get("#newNavMenu").type("Contact Us");
+    cy.get("#newNavLink").type("/contact");
+    cy.get("#settings-menu-new").click();
+    cy.get("#notify_message").should("have.text", "Menu created successfully");
   });
-  it("Sin nombre", () => {
+  it("Nombre numero y link bien", () => {
     cy.get("#email").type("juanestebanortizbejarano@gmail.com");
     cy.get("#password").type("admin12");
     cy.get("#loginForm").click();
     cy.get(".mb-2 > :nth-child(2) > .nav-link").click();
-    cy.get("#newNavMenu");
-    cy.get("#newNavLink");
-    cy.get("#settings-menu-new");
-    cy.get("#notify_message");
+    cy.get("#newNavMenu").type(12);
+    cy.get("#newNavLink").type("/contact");
+    cy.get("#settings-menu-new").click();
+    cy.get("#notify_message").should("have.text", "Name cannot be a number");
   });
-  it("Nombre por fuera de la longitud aceptada", () => {
+  it("Sin nombre y link bien", () => {
     cy.get("#email").type("juanestebanortizbejarano@gmail.com");
     cy.get("#password").type("admin12");
     cy.get("#loginForm").click();
     cy.get(".mb-2 > :nth-child(2) > .nav-link").click();
-    cy.get("#newNavMenu");
-    cy.get("#newNavLink");
-    cy.get("#settings-menu-new");
-    cy.get("#notify_message");
+    cy.get("#newNavMenu").type("Contact Us");
+    // cy.get("#newNavLink").type("");
+    cy.get("#settings-menu-new").click();
+    cy.get("#notify_message").should("have.text", "Link is empty");
+  });
+  it("Nombre bien y link numero", () => {
+    cy.get("#email").type("juanestebanortizbejarano@gmail.com");
+    cy.get("#password").type("admin12");
+    cy.get("#loginForm").click();
+    cy.get(".mb-2 > :nth-child(2) > .nav-link").click();
+    cy.get("#newNavMenu").type("Contact Us");
+    cy.get("#newNavLink").type(2);
+    cy.get("#settings-menu-new").click();
+    cy.get("#notify_message").should("have.text", "Link cannot be a number");
+  });
+  it("Nombre vacio y link bien", () => {
+    cy.get("#email").type("juanestebanortizbejarano@gmail.com");
+    cy.get("#password").type("admin12");
+    cy.get("#loginForm").click();
+    cy.get(".mb-2 > :nth-child(2) > .nav-link").click();
+    //cy.get("#newNavMenu").type("");
+    cy.get("#newNavLink").type("/contact");
+    cy.get("#settings-menu-new").click();
+    cy.get("#notify_message").should("have.text", "Name is empty");
   });
 });
